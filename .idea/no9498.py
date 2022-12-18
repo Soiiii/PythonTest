24d223b58505145115171c8a0cdd08e8827ac03a
Soiiii <cojomkr33@gmail.com>
2022-12-18 22:32:15 +0900

    백준 9498번

---
+++ .idea/no9498.py
@@ -0,0 +1,13 @@
+class no1000:
+    # 9498번 시험성적
+    A = int(input())
+    if 90<= A <=100:
+        print("A")
+    elif 80<= A < 90:
+        print("B")
+    elif 70<= A < 80:
+        print("C")
+    elif 60<= A < 70:
+        print("D")
+    else:
+        print("F")
