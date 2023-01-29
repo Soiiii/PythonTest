01871f60124a5481fce86a14fc040ccaf961eabe
Soiiii <cojomkr33@gmail.com>
2023-01-29 22:39:31 +0900

    coursera week2

---
+++ UsingDatabaseswithPython/Week2_Using_Databases.py
@@ -0,0 +1,19 @@
+# Two Roles in Large Projects
+# Application Developer - Builds the logic for the application, the look and feel of the application
+# - monitors the application for problems
+
+# Database Administrator - Monitor and adjusts the databases as the program runs in production
+# Often both people participate in the building of the "Data Model"
+
+# Database Model
+# A database model or database schema is the structure or format of a database,
+# described in a formal language supported by the database management system.
+# In other words, a "database model" is the application of a data model when used in conjunction
+# with a database management system.
+
+# Common Database Systems
+# Oracle - Large, commercial, enterprise-scale, very very tweakable -> enterprise
+# MySql - Simpler but very fast and scalable - commercial open source -> website
+# SqlServer -ver nice from Microsoft(Also Access)
+# Postgres - similar to Oracle
+# SqlLite - small, goes in to the application, fast -> python import
