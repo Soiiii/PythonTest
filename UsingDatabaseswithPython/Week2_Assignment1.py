76343f1a3cb4a079d504f7f59d46544d9eb1dd73
Soiiii <cojomkr33@gmail.com>
2023-01-30 21:10:29 +0900

    coursera week2

--- UsingDatabaseswithPython/Week2_Assignment1.py
+++ UsingDatabaseswithPython/Week2_Assignment1.py
@@ -1,3 +1,13 @@
+# Then, create a SQLITE database or use an existing database and create a table in the database called "Ages":
+# Then make sure the table is empty by deleting any rows that you previously inserted, and
+# insert these rows and only these rows with the following commands:
+# Once the inserts are done, run the following SQL command:
+# Find the first row in the resulting record set and enter the long string that looks like
+# 53656C696E613333.
+
+
+
+
 import sqlite3
 
 conn = sqlite3.connect('Ages.sqlite')
@@ -12,5 +22,8 @@ cur.executemany('INSERT INTO Ages VALUES(?,?);', records)
 
 cur.execute('SELECT hex(name || age) AS X FROM Ages ORDER BY X')
 print(cur.fetchall())
+#[('43656C73693136',), ('4368657272793338',), ('47726569673231',), ('4A656576616E3338',), ('4B7361776572793338',), ('56697267696E69653136',)]
+
+
 conn.commit()
 conn.close()
