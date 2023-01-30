8580837afa981ad510da4bf9735f9482b63be875
Soiiii <cojomkr33@gmail.com>
2023-01-31 05:59:37 +0900

    coursera week3

---
+++ UsingDatabaseswithPython/Week3_Designing_a_data_model.py
@@ -0,0 +1,12 @@
+# Database Design
+# Database design is an art form of its own with particular skiils and experience
+# Our goal is to avoid the really bad mistakes and design clean and easily understood database
+# Others may performance tune things later
+# Database design starts with a picture
+
+# Building a Data Model
+# Drawing a picture of the data objects for our application and then figuring out how to represent the objects and
+# their relationships
+# Basic Rule: Don't put the same sting data in twice - use a relationship instead
+# When there is one thing in the "real world" there should be one copy of that thing in the database
+
