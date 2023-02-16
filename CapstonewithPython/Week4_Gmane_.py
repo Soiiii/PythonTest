28be15ff824381472745d3b45e82447565845ee9
Soiiii <cojomkr33@gmail.com>
2023-02-17 06:25:08 +0900

    coursera week4

---
+++ CapstonewithPython/Week4_Gmane_.py
@@ -0,0 +1,6 @@
+# Mailing Lists - Gmane
+# Crawl the archive of a mailing list
+# Do some analysis/cleanup
+# Visualize the data as word cloud and lines
+# http://mbox.dr-chuck.net/sakai.devel/4/5
+
