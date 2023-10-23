018f35e8e9ac0e6bf6516c5b46debe2b12061f69
Soiiii <cojomkr33@gmail.com>
2023-10-23 23:06:01 +0900

    프로그래머스 코테

--- Test/test7.py
+++ Test/test7.py
@@ -2,10 +2,18 @@
 # 두 정수 a, b와 boolean 변수 flag가 매개변수로 주어질 때,
 # flag가 true면 a + b를 false면 a - b를 return 하는 solution 함수를 작성해 주세요.
 
-def solution(a, b, flag):
+def solution(a, d, included):
     answer = 0
-    if(flag is True):
-        answer = a + b
-    else:
-        answer = a - b
+    number = 0
+    for i in included:
+        if 'True' in str(i):
+            answer = answer + a + (d*(number))
+        number += 1
+    return answer
+
+# 나은 답
+def solution(a, d, included):
+    answer = 0
+    for i in range(len(included)):
+        answer += (a + d * i) * int(included[i])
     return answer
