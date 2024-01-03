be3e4007c94b6a9ce07c0f39ba9886e01368e86d
Soiiii <cojomkr33@gmail.com>
2024-01-03 23:43:22 +0900

    프로그래머스 코테

---
+++ Test/test52.py
@@ -0,0 +1,17 @@
+# The Number of p and y in String
+# Suppose that a string s consisting of both lower-case and upper-case
+# letters are given. Write a function "solution" to return True when
+# the number of 'p' and 'y' in s are the same, and return False otherwise.
+# Note that when there is neither 'p' nor 'y', it should always return True.
+# Also, when comparing the number of 'p' and 'y', lower-case and upper-case
+# are not distinguished.
+# For example, if s is "pPoooyY", then return true. In the case of "Pyy",
+# return false.
+
+def solution(s):
+    answer = True
+
+    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    print('Hello Python')
+
+    return True
