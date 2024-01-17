55b51110abb6009c19439c0e5c41403f88278598
Soiiii <cojomkr33@gmail.com>
2024-01-17 23:46:37 +0900

    프로그래머스 코테

---
+++ Test/test59.py
@@ -0,0 +1,15 @@
+# 2의 영역
+# 정수 배열 arr가 주어집니다. 배열 안의 2가 모두 포함된 가장 작은 연속된 부분 배열을
+# return 하는 solution 함수를 완성해 주세요.
+# 단, arr에 2가 없는 경우 [-1]을 return 합니다.
+
+def solution(arr):
+    answer = []
+    for a in arr:
+
+    else:
+        answer.append(-1)
+    return answer
+
+solution([1, 2, 1, 4, 5, 2, 9])
+# print(solution([1, 1, 1]))
