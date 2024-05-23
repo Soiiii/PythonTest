578fd81d6fc22bdac7759a012bb0bec50d590535
Soiiii <cojomkr33@gmail.com>
2024-05-23 14:02:29 +0900

    프로그래머스 코테

--- Test/test126.py
+++ Test/test126.py
@@ -4,19 +4,32 @@
 # 여벌의 체육복을 가져온 학생들의 번호가 담긴 배열 reserve가 매개변수로 주어질 때,
 # 체육수업을 들을 수 있는 학생의 최댓값을 return 하도록 solution 함수를 작성해주세요.
 
-def solution(n, lost, reserve):
-    lost.sort()
-    reserve.sort()
-    for i in reserve:
-        print('i', i)
-        if i in lost:
-            reserve.remove(i)
-            lost.remove(i)
-    for i in reserve:
-        if i -1 in lost:
-            lost.remove(i-1)
-        elif i + 1 in lost:
-            lost.remove(i+1)
+# def solution(n, lost, reserve):
+#     lost.sort()
+#     reserve.sort()
+#     for i in reserve:
+#         print('i', i)
+#         if i in lost:
+#             reserve.remove(i)
+#             lost.remove(i)
+#     for i in reserve:
+#         if i -1 in lost:
+#             lost.remove(i-1)
+#         elif i + 1 in lost:
+#             lost.remove(i+1)
+#
+#     answer = n - len(lost)
+#     return answer
 
-    answer = n - len(lost)
-    return answer
+def solution(n, lost, reserve):
+    reserve_set = set(reserve) - set(lost)
+    lost_set = set(lost) - set(reserve)
+
+    for i in reserve_set:
+        if i-1 in lost_set:
+            lost_set.remove(i-1)
+        elif i+1 in lost_set:
+            lost_set.remove(i+1)
+    return n-len(lost_set)
+
+solution(5,[2, 4],[1, 3, 5])
