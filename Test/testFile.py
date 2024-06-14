23d26486696176124a39c66b1944252dab1e0286
Soiiii <cojomkr33@gmail.com>
2024-06-14 23:45:51 +0900

    파일 오픈 테스트

--- Test/testFile.py
+++ Test/testFile.py
@@ -1,16 +1,31 @@
-# import csv
-#
-# file_path = ''
-#
-# with open(file_path, mode='r', encoding='utf-8') as file:
-#     csv_reader = csv.reader(file)
-#     for row in csv_reader:
-#         print(row)
 
-import pandas as pd
+data = b'Hello World\r\n'
 
-file_path = ''
+# 바이트코드 -> 텍스트
+text = data.decode('utf-8')
+print(text)
+# 텍스트 -> 바이트
+data = text.encode('utf-8')
+print(data)
+# f-String f문자열
+name = 'IBM'
+shares = 100
+price = 91.1
+a = f'{name:10s} {shares:10d} {price:10.2f}'
+b = f'{name} {shares} {price}'
+print(a)
+print(b)
+c = f'Cost = ${shares * price:0.2f}'
+print(c)
 
-df = pd.read_csv(file_path)
+names = [ 'Elwood', 'Jake', 'Curtis' ]
+nums = [ 39, 38, 42, 65, 111]
+line = 'GOOG,100,490.10'
+row = line.split(',')
+row.insert(2,'aa') #['GOOG', '100', 'aa', '490.10']
+print(row)
 
-print(df)
+names.remove('Elwood')
+print(names)
+del names[0]
+print(names)
