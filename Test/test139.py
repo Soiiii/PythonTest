89f487ff871203352cfde5f12c266bf69f9e7e58
Soiiii <cojomkr33@gmail.com>
2024-06-17 23:46:59 +0900

    프로그래머스 테스트

--- Test/test139.py
+++ Test/test139.py
@@ -1,13 +1,10 @@
 
-# 중복된 숫자 개수
+# 중앙값 구하기
 # 정수가 담긴 배열 array와 정수 n이 매개변수로 주어질 때,
 # array에 n이 몇 개 있는 지를 return 하도록 solution 함수를 완성해보세요.
-def solution(array, n):
-    return array.count(n)
-
-def solution(array, n):
+def solution(array):
     answer = 0
-    for j in array:
-        if n == j:
-            answer += 1
+    array.sort()
+    print(array)
+    answer = array[int(len(array) / 2)]
     return answer
