1e22bd3c5a57d8bc3e3ea836e7e9e8dfe7a4bfbd
Soiiii <cojomkr33@gmail.com>
2024-08-05 23:57:28 +0900

    프로그래머스 테스트

--- Test/test120.py
+++ Test/test120.py
@@ -5,14 +5,32 @@
 # 마라톤에 참여한 선수들의 이름이 담긴 배열 participant와 완주한 선수들의 이름이 담긴 배열
 # completion이 주어질 때,
 # 완주하지 못한 선수의 이름을 return 하도록 solution 함수를 작성해주세요.
-from collections import Counter
 def solution(participant, completion):
-    participant_counter = Counter(participant)
-    completion_counter = Counter(completion)
+    answer = ''
+    hash_dic = {}
+    sum = 0
+    for i in participant:
+        hash_dic[hash(i)] = i
+        sum += hash(i)
+    for j in completion:
+        sum -= hash(j)
+    return hash_dic[sum]
+    # participant.sort()
+    # completion.sort()
+    # for i,j in zip(participant, completion):
+    #     print(i,j)
+    #     if i != j:
+    #         return i
 
-    difference = participant_counter - completion_counter
-    print(difference.keys())
-    return list(difference.keys()[0])
+solution(["marina", "josipa", "nikola", "vinko", "filipa"], ["josipa", "filipa", "marina", "nikola"])
+# from collections import Counter
+# def solution(participant, completion):
+#     participant_counter = Counter(participant)
+#     completion_counter = Counter(completion)
+#
+#     difference = participant_counter - completion_counter
+#     print(difference.keys())
+#     return list(difference.keys()[0])
 
     # answer = ''
     # for j in participant:
